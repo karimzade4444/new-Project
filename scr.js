b336aeacfd243ce6d3ff801f415dbcf747965440
Karimzoda Mustafo <karimzademustafa@gmail.com>
2026-04-05 21:29:55 +0500

    Get

--- scr.js
+++ scr.js
@@ -1,1 +1,7 @@
 let api = "https://69d2798a5043d95be971eb73.mockapi.io/api/data"
+
+
+
+let container = document.querySelector(".container")
+let middle = document.querySelector(".middle")
+let search = document.querySelector(".search")
